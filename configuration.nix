fef60e1a87ba94fc2590424e709c0b253d724715
APnda <apnda6661@proton.me>
2024-02-25 16:07:58 -0600

    Add settings to tlp

--- configuration.nix
+++ configuration.nix
@@ -79,7 +79,21 @@
   ##programs.waybar.enable = true;
 
   services = {
-    tlp.enable = true;
+    tlp = {
+      enable = true;
+      settings = {
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "schedutil";
+
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+        CPU_MIN_PERF_ON_AC = 0;
+        CPU_MAX_PERF_ON_AC = 100;
+        CPU_MIN_PERF_ON_BAT = 0;
+        CPU_MAX_PERF_ON_BAT = 80;
+      };
+    };
   };
 
 
