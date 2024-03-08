04eaaac533fad5cee43392d14019faaa09b08219
APnda <apnda6661@proton.me>
2024-03-08 10:27:19 -0600

    Changed gtk theme to not black

--- user/home.nix
+++ user/home.nix
@@ -18,7 +18,7 @@
         package = pkgs.catppuccin-gtk.override {
           accents = [ "mauve" ];
           size = "compact";
-          tweaks = [ "rimless" "black" ];
+          tweaks = [ "rimless" ];
           variant = "macchiato";
         };
       };
