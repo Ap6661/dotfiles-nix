5e2a6a119d8c69cfa9e6d342d69b558236cb8386
APnda <apnda6661@proton.me>
2024-03-09 13:30:33 -0600

    Bluetooth and capslock switch

--- hardware/gpd.nix
+++ hardware/gpd.nix
@@ -1,6 +1,7 @@
 { pkgs, nixos-hardware, ... }:
 {
   hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
 
   services.xserver.displayManager.sessionCommands = ''
   keyboardId=$(
@@ -8,7 +9,7 @@
       sed -nE 's/.*AT Translated Set 2 keyboard.*?id=([0-9]+).*/\1/p'
     )
     if [[ $keyboardId ]]; then 
-      ${pkgs.xorg.setxkbmap}/bin/setxkbmap -device $keyboardId -option ctrl:nocaps
+      ${pkgs.xorg.setxkbmap}/bin/setxkbmap -device $keyboardId -option ctrl:swapcaps
     fi
   '';
 }
