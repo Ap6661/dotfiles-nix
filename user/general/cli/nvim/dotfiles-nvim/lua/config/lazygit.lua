a16b16b3ac4d552c8a6a2730cae548c3329ee7e7
APnda <apnda6661@proton.me>
2024-03-18 12:48:15 -0500

    Fix Submodule

--- user/general/cli/nvim/dotfiles-nvim/lua/config/lazygit.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "kdheepak/lazygit.nvim",
-  -- optional for floating window border decoration
-  dependencies = { "nvim-lua/plenary.nvim", },
-  config = function ()
-    vim.keymap.set('n', '<Leader><f1>', ':LazyGit<CR>', { noremap = true, silent = true } )
-  end
-}
