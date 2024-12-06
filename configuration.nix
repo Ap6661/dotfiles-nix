8ecd19c32ed0a6f4a7db5c2fa85fb207aca2daf0
APnda <apnda6661@proton.me>
2024-12-05 18:02:13 -0600

    Cleanup

--- configuration.nix
+++ configuration.nix
@@ -68,9 +68,13 @@
 #jack.enable = true;
   };
 
+  boot = {
 # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+  };
 
   networking.hostName = "nixos"; # Define your hostname.
 # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
