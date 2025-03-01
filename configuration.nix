5ac0bdafeac4413be300b59a3cb09f9dcb305b50
APnda <apnda6661@proton.me>
2025-03-01 05:37:58 -0600

    Specialisations

--- configuration.nix
+++ configuration.nix
@@ -1,35 +1,57 @@
 { pkgs, config, ... }:
+let
+c = {
+  base00 = "#000000";
+  base01 = "#1a0c14";
+  base02 = "#321c29";
+  base03 = "#533144";
+  base04 = "#7c4d68";
+  base05 = "#af6e92";
+  base06 = "#cda3b9";
+  base07 = "#ecdfe6";
+  base08 = "#e07d8b";
+  base09 = "#e0957e";
+  base0A = "#e0c87e";
+  base0B = "#84e07e";
+  base0C = "#7ee0d8";
+  base0D = "#7e8ee0";
+  base0E = "#e07ece";
+  base0F = "#b26e44";
+};
+in
 {
   imports =
     [
-    ./user/x/i3/i3.nix
+      ./user/x/i3/i3.nix
       ./user/x/rofi/rofi.nix
       ./user/general/cli/btop/btop.nix
       ./user/general/cli/zsh/zsh.nix
       ./user/general/gui/messaging/messaging.nix
       ./user/general/gui/wezterm/wezterm.nix
+      ./special.nix
     ];
   stylix = {
     enable = true;
-    base16Scheme = {
-      base00 = "#000000";
-      base01 = "#1a0c14";
-      base02 = "#321c29";
-      base03 = "#533144";
-      base04 = "#7c4d68";
-      base05 = "#af6e92";
-      base06 = "#cda3b9";
-      base07 = "#ecdfe6";
-      base08 = "#e07d8b";
-      base09 = "#e0957e";
-      base0A = "#e0c87e";
-      base0B = "#84e07e";
-      base0C = "#7ee0d8";
-      base0D = "#7e8ee0";
-      base0E = "#e07ece";
-      base0F = "#b26e44";
+    base16Scheme = pkgs.lib.mkIf (config.specialisation != {}) 
+    {
+      base00 = c.base00;
+      base01 = c.base01;
+      base02 = c.base02;
+      base03 = c.base03;
+      base04 = c.base04;
+      base05 = c.base05;
+      base06 = c.base06;
+      base07 = c.base07;
+      base08 = c.base08;
+      base09 = c.base09;
+      base0A = c.base0A;
+      base0B = c.base0B;
+      base0C = c.base0C;
+      base0D = c.base0D;
+      base0E = c.base0E;
+      base0F = c.base0F;
     };
-    image = config.lib.stylix.pixel "base03";
+    image = pkgs.lib.mkIf (config.specialisation != {}) ./user/theme/background;
     # image = ./user/theme/background;
     # image = pkgs.runCommand "image.png" {} '' 
     #   ${pkgs.imagemagick}/bin/magick convert ${./user/theme/bg.jpg} -fill "${config.stylix.base16Scheme.base00}" -colorize 60 $out
@@ -75,6 +97,7 @@
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
+    binfmt.emulatedSystems = [ "armv7l-linux" ];
   };
 
   networking.hostName = "nixos"; # Define your hostname.
---
+++ special.nix
@@ -0,0 +1,69 @@
+{ pkgs, lib, config, ... }: {
+  specialisation = lib.lists.foldr (a: b: a // b) {} (lib.lists.forEach
+    [
+    { 
+      name = "gruvbox";
+      base00 = "#282828";
+      base01 = "#3c3836";
+      base02 = "#504945";
+      base03 = "#665c54";
+      base04 = "#bdae93";
+      base05 = "#d5c4a1";
+      base06 = "#ebdbb2";
+      base07 = "#fbf1c7";
+      base08 = "#fb4934";
+      base09 = "#fe8019";
+      base0A = "#fabd2f";
+      base0B = "#b8bb26";
+      base0C = "#8ec07c";
+      base0D = "#83a598";
+      base0E = "#d3869b";
+      base0F = "#d65d0e"; 
+    }
+    { 
+      name = "mocha";
+      base00 = "#3B3228";
+      base01 = "#534636";
+      base02 = "#645240";
+      base03 = "#7e705a";
+      base04 = "#b8afad";
+      base05 = "#d0c8c6";
+      base06 = "#e9e1dd";
+      base07 = "#f5eeeb";
+      base08 = "#cb6077";
+      base09 = "#d28b71";
+      base0A = "#f4bc87";
+      base0B = "#beb55b";
+      base0C = "#7bbda4";
+      base0D = "#8ab3b5";
+      base0E = "#a89bb9";
+      base0F = "#bb9584"; 
+    }
+  ] 
+    (c: 
+     {
+       ${c.name}.configuration = { 
+         config.stylix = {
+           base16Scheme = {
+             base00 = c.base00;
+             base01 = c.base01;
+             base02 = c.base02;
+             base03 = c.base03;
+             base04 = c.base04;
+             base05 = c.base05;
+             base06 = c.base06;
+             base07 = c.base07;
+             base08 = c.base08;
+             base09 = c.base09;
+             base0A = c.base0A;
+             base0B = c.base0B;
+             base0C = c.base0C;
+             base0D = c.base0D;
+             base0E = c.base0E;
+             base0F = c.base0F;
+         };
+         image = pkgs.runCommand "image.png" {} "${pkgs.imagemagick}/bin/magick -size 1x1 xc:${c.base03} $out\n";
+       };
+     }; 
+     }));
+}
