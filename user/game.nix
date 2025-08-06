8605cf38bd8ef45fc2102e69582b107c931cbbb2
APnda <apnda6661@proton.me>
2025-08-06 09:57:20 -0500

    Add Proton GE

--- user/game.nix
+++ user/game.nix
@@ -2,6 +2,9 @@
 {
   nixpkgs.config.allowBroken = true;
   programs.steam.enable = true;
+  programs.steam.extraCompatPackages = with pkgs; [
+    proton-ge-bin
+  ];
   environment.systemPackages = with pkgs; [
     lutris
     # itch
