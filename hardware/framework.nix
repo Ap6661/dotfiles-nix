487694717b109e52388c67a1d289fcc066f0dd59
APnda <apnda6661@proton.me>
2025-09-03 15:05:13 -0500

    Add kanata

--- hardware/framework.nix
+++ hardware/framework.nix
@@ -29,14 +29,77 @@
   ## 
   ## As well as normal scrolling
   ## 
-  services.xserver.displayManager.sessionCommands = ''
-     keyboardId=$(
-       ${pkgs.xorg.xinput}/bin/xinput list --id-only 'AT Translated Set 2 keyboard'
-       )
-       if [[ $keyboardId ]]; then 
-         ${pkgs.xorg.setxkbmap}/bin/setxkbmap -device $keyboardId -option ctrl:nocaps
-       fi
+     # keyboardId=$(
+     #   ${pkgs.xorg.xinput}/bin/xinput list --id-only 'AT Translated Set 2 keyboard'
+     #   )
+     #   if [[ $keyboardId ]]; then 
+     #     ${pkgs.xorg.setxkbmap}/bin/setxkbmap -device $keyboardId -option ctrl:nocaps
+     #   fi
 
+  services.kanata = {
+    enable = true;
+    keyboards = {
+      internalKeyboard = {
+        devices = [ "/dev/input/by-path/platform-i8042-serio-0-event-kbd" ];
+        config = ''
+          (defsrc
+           grv  1    2    3    4    5    6    7    8    9    0    -    =    bspc
+           tab  q    w    e    r    t    y    u    i    o    p    [    ]    \
+           caps a    s    d    f    g    h    j    k    l    ;    '    ret
+           lsft z    x    c    v    b    n    m    ,    .    /    rsft
+           lctl lmet lalt           spc            ralt rctl
+          )
+
+          (defalias
+            osh (one-shot 500 lsft)
+            ors (one-shot 500 rsft)
+            ect (tap-hold 100 100 esc lctl)
+
+            al (tap-hold 200 200 (one-shot 200 ralt) (layer-while-held arrow))
+
+            < S-,
+            > S-.
+            ~ S-grv
+
+            sqn (macro @> @~ @<)
+            abr (macro @< @> left)
+
+            gam (layer-switch gaming)
+            bas (layer-switch base)
+
+          )
+
+          (deflayer base
+           grv  1    2    3    4    5    6    7    8    9    0    -    =    bspc
+           tab  q    w    e    r    t    y    u    i    o    p    [    ]    \
+           @ect a    s    d    f    g    h    j    k    l    ;    '    ret
+           @osh z    x    c    v    b    n    m    ,    .    /    @ors
+           lctl lmet lalt           spc            @al  rctl
+          )
+
+          (deflayer arrow
+           grv S-1  S-2  S-3  S-4  S-5  S-6  S-7  S-4   S-9  S-0    -    =    bspc
+           tab  1    2    3    4    5    6    7    8     9    0    [    ]    \
+           @ect a    s    d @sqn    g left down   up right    ;    '    ret
+           @osh @gam @abr    c    v    b    n    m    ,     .    /    rsft
+           lctl lmet lalt           spc            ralt rctl
+          )
+
+          (deflayer gaming
+           grv  1    2    3    4    5    6    7    8    9    0    -    =    bspc
+           tab  q    w    e    r    t    y    u    i    o    p    [    ]    \
+           lctl a    s    d    f    g    h    j    k    l    ;    '    ret
+           lsft z    x    c    v    b    n    m    ,    .    /    rsft
+           lctl esc lalt           spc            @bas  rctl
+          )
+          '';
+
+      };
+    };
+
+  };
+
+  services.xserver.displayManager.sessionCommands = ''
        ${pkgs.xorg.xinput}/bin/xinput set-prop PIXA3854:00\ 093A:0274\ Touchpad libinput\ Natural\ Scrolling\
     Enabled 1
   '';
