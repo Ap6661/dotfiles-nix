d8b5ab05e43458b030ef8920c41283ed0d55d5da
APnda <apnda6661@proton.me>
2026-02-02 10:34:33 -0600

    update borders, use i3 2.25
    fixup

--- user/x/i3/i3.nix
+++ user/x/i3/i3.nix
@@ -11,7 +11,25 @@ in
     services.xserver = {
       windowManager.i3 = {
         enable = true;
-        package = pkgs.i3;
+        package = pkgs.i3.overrideAttrs (old: { 
+            version = "4.25"; 
+            src = pkgs.fetchFromGitHub {
+              owner = "i3";
+              repo = "i3";
+              tag = "4.25";
+              hash = "sha256-bkfuzy7ewHwA9GQNbUvZjQVCCYrnnm2Wf/htHRSkdzk=";
+            };
+            postPatch = ''
+              patchShebangs .
+
+              # This testcase generates a Perl executable file with a shebang, and
+              # patchShebangs can't replace a shebang in the middle of a file.
+              if [ -f testcases/t/318-i3-dmenu-desktop.t ]; then
+              substituteInPlace testcases/t/318-i3-dmenu-desktop.t \
+              --replace-fail "#!\$Config{perlpath}" "#!${lib.getExe pkgs.perl}"
+              fi
+            '';
+        });
         extraPackages = with pkgs; [
           nitrogen
           i3lock
--- user/x/i3/picom/xx0x.glsl
+++ user/x/i3/picom/xx0x.glsl
@@ -1,6 +1,6 @@
 #version 330
 in vec2 texcoord;             // texture coordinate of the fragment
-uniform float corner_radius;  // corner radius of the window (pixels)
+// uniform float corner_radius;  // corner radius of the window (pixels)
 uniform float opacity;  
 uniform float dim;  
 uniform float border_width;  
@@ -25,11 +25,13 @@ vec4 window_shader() {
     // vec4 color = texture(tex, texcoord);
     vec4 color = texelFetch(tex, ivec2(texcoord), 0);
 
-    // float borderWidth = 10.0;
-    float borderWidth = border_width;
+    int corner_radius = 20;
+    float borderWidth = 3.0;
+    // float borderWidth = border_width; // Can't use this because picom gets the top as the border width 
     vec2 texSize = textureSize(tex, 0);
 
-    float sdf = sdRoundedBox(texcoord, texSize, corner_radius);
+    vec2 offset= vec2(0.0, corner_radius);
+    float sdf = sdRoundedBox(texcoord+offset, texSize+offset, corner_radius);
 
     if (sdf >= -borderWidth && sdf <= 0) {
 
@@ -40,28 +42,25 @@ vec4 window_shader() {
 
       ivec2 coords = ivec2(texSize.x/2, texSize.y - 1);
 
-      if (px.y < halfheight) 
-      {
-        coords = ivec2(texSize.x/2, 1);
-      }
-      if (px.x < halfwidth && abs(halfheight - px.y) <= halfheight - px.x) 
+      if (px.x < halfwidth && ((abs(halfheight - px.y) <= halfheight - px.x) || px.y < halfheight))
       {
         coords = ivec2(1, texSize.y/2);
       } 
-      else if (px.x > halfwidth && halfheight - abs(halfheight - px.y) >= wh.x - px.x) 
+      else if (px.x > halfwidth && (halfheight - abs(halfheight - px.y) >= wh.x - px.x || px.y < halfheight)) 
       {
         coords = ivec2(texSize.x - 1, texSize.y/2);
       } 
-      else if (px.y < halfheight) 
-      {
-        coords = ivec2(texSize.x/2, 1);
-      }
 
       vec4 c = default_post_processing(vec4 (0.0, 0.0, 0.0, 0.0));
       c.rgb = texelFetch(tex, coords, 0).rgb;
       return c;
+      // return vec4 (1.0, mod(sdf / borderWidth, 1.0), 1.0, 1.0);
     }
-  
-    // Apply default post-processing
-    return default_post_processing(color);
+
+    if (sdf < 0.0) {
+      // return vec4 (0.0, 1.0, 0.0, 1.0);
+      return default_post_processing(color);
+    }
+    return default_post_processing(vec4(0.0, 0.0, 0.0, 0.0));
+    return vec4(0.0, 0.0, 0.0, 0.0);
 }
