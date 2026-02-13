c88bd57c23cabae53567e09150aface914ae28ac
APnda <apnda6661@proton.me>
2026-02-13 18:56:17 +0000

    network connection is important....

--- modules/nixosModules/core.nix
+++ modules/nixosModules/core.nix
@@ -20,6 +20,31 @@
 
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+
+  networking = {
+	  hostName = "nixos"; # Define your hostname.
+	  networkmanager.enable = true;
+  };
+
+  	time.timeZone = "American/Chicago";
+	i18n = { 
+		defaultLocale = "en_US.UTF-8";
+		extraLocaleSettings = {
+			LC_ADDRESS = "en_US.UTF-8";
+			LC_IDENTIFICATION = "en_US.UTF-8";
+			LC_MEASUREMENT = "en_US.UTF-8";
+			LC_MONETARY = "en_US.UTF-8";
+			LC_NAME = "en_US.UTF-8";
+			LC_NUMERIC = "en_US.UTF-8";
+			LC_PAPER = "en_US.UTF-8";
+			LC_TELEPHONE = "en_US.UTF-8";
+			LC_TIME = "en_US.UTF-8";
+		};
+
+	};
+
+	xdg.sounds.enable = false;
+
     system.stateVersion = "23.11"; # Don't Change This
   };
 }
