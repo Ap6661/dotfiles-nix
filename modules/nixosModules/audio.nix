a518eba0dde912bc151cbdb103ac923a4e54c8d9
APnda <apnda6661@proton.me>
2026-02-16 11:29:49 -0600

    audio

---
+++ modules/nixosModules/audio.nix
@@ -0,0 +1,12 @@
+{ ... }:
+{
+  flake.nixosModules.core = {
+    security.rtkit.enable = true;
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+    };
+  };
+}
