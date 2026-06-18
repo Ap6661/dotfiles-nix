e9d43bac40223d0622df379e843c919c4a41bb44
APnda <apnda6661@proton.me>
2026-06-17 23:47:58 -0500

    Fix audio

--- modules/hosts/nixos/hardware-configuration.nix
+++ modules/hosts/nixos/hardware-configuration.nix
@@ -25,6 +25,18 @@
       boot.kernelModules = [ "kvm-intel" ];
       boot.extraModulePackages = [ ];
 
+      systemd.services.disable-snd-hda-power-save = {
+        description = "Disable snd_hda_intel power_save";
+        after = ["sound.target"];
+        wants = ["sound.target"];
+        serviceConfig = {
+          Type = "oneshot";
+          ExecStart = "/bin/sh -c 'echo 0 > /sys/module/snd_hda_intel/parameters/power_save'";
+          RemainAfterExit = true;
+        };
+        wantedBy = ["multi-user.target"];
+      };
+
       fileSystems."/" = {
         device = "/dev/disk/by-uuid/79bcf5e9-d6b7-4b39-b509-41336f8fbd55";
         fsType = "ext4";
