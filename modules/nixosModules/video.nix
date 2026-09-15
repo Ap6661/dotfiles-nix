8b0d1f3ae469d40b7ba4b7fa49de70b4b95f06c4
APnda <apnda6661@proton.me>
2026-09-15 13:28:10 -0500

    ani-cli (custom version number)

--- modules/nixosModules/video.nix
+++ modules/nixosModules/video.nix
@@ -16,6 +16,16 @@
           ];
         })
         yt-dlp
+        (ani-cli.overrideAttrs (old: {
+          version = "5.1";
+          src = fetchFromGitHub {
+            owner = "pystardust";
+            repo = "ani-cli";
+            tag = "v5.1";
+            hash = "sha256-lPQA3iO3F/9NS2IziQccsJ3aai6WMQy6YObdB3mDCZA=";
+          };
+
+        }))
       ];
     };
 }
